0edf16978d2a77e84f1cd38d2decd9d77672eede
WojtekMsi <WojtekMsi@WOJTEK_MSIDESK>
2026-03-24 00:30:40 +0100

    CourseDetaillookbetter

--- frontend/src/pages/CourseDetail.tsx
+++ frontend/src/pages/CourseDetail.tsx
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom';
 import { courses } from '../data';
+import '../assets/styles/coursedetail.css'; 
 
 export default function CourseDetailPage() {
   const { id } = useParams<{ id: string }>();
