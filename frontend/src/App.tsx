8fcc2ecfb4521ac3c6bf5a048d7021a99fd2850f
WojtekMsi <WojtekMsi@WOJTEK_MSIDESK>
2026-03-23 23:52:06 +0100

    LessonSection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import HomePage from './pages/Home';
 import CoursePage from './pages/CoursePage'; 
 import CourseDetailPage from './pages/CourseDetail';
+import LessonSection from './pages/LessonSection';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           {/* TO JEST KLUCZOWE: */}
           <Route path="/katalog" element={<CoursePage />} /> 
+          <Route path="/nauka" element={<LessonSection />} /> 
           <Route path="/course/:id" element={<CourseDetailPage />} />
         </Routes>
       </main>
